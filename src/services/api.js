5a6885e58390e39557f2d81fe24018e259909038
yuvraj <yuvraj042003@gmail.com>
2024-03-03 09:03:04 +0530

    push fronmtend

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL:`http://localhost:8000/api/v1`});
+API.interceptors.request.use((req)=>{
+    if(localStorage.getItem('token')){
+        req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    }
+    return req;
+});
+
+export default API;
